58b04500361d3116d365c2d69e93b65d9695b73f
Sam Whang <sangwoowhang@gmail.com>
2017-10-06 16:34:59 -0500

    Added parser template class

---
+++ Knockout/Parser.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace Knockout
+{
+
+    public class Parser
+    {
+        private readonly IEnumerable<Movie> movies;
+
+        public Parser()
+        {
+            // get html data
+        }
+
+        class Movie
+        {
+            public string city { get; set; }
+            public string times { get; set; }
+            public string titles { get; set; }
+            public string rating { get; set; }
+            public string length { get; set; }
+        }
+
+    }
+}
